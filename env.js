c49356142e8b3927b4186f7c64b73c304356d5f9
MoeTutorial <140126768+MoeTutorial@users.noreply.github.com>
2024-10-28 15:04:32 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://back.fengniaoyun.site/'
+window.settings = {
+  // 站点名称
+  title: '蜂鸟云',
+  // 站点描述
+  description: '快速、自由的网络连接方式',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'default', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: 'https://bing.img.run/uhd.php',
+  // 站点LOGO
+  logo: '',
+}
